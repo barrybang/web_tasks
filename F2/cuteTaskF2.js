91a9420f509663715a6eae35000241f4600454f6
barrybang <2668212523@qq.com>
2016-08-11 23:40:12 +0800

    更改：text和：password

--- F2/cuteTaskF2.js
+++ F2/cuteTaskF2.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
   $(".usernameBox").click(function(){
-    $(":text").animate({height:26},400);
+    $(this).children("input").animate({height:30},400);
     $("label.userName").animate({fontSize:12,left:0,top:0},200);
-    $("label.userName").stop().animate({fontSize:12,left:0,top:6},300);
+    $("label.userName").stop().animate({fontSize:12,left:0,top:5},300);
   })
-  $(":text").blur(function(){
-    var usernameArr=$(":text").val();
+  $(".usernameBox").children("input").blur(function(){
+    var usernameArr=$(this).val();
     if (usernameArr.length == 0) {
-      $(":text").animate({height:2},400);
+      $(this).animate({height:2},400);
       $("label.userName").stop().animate({fontSize:18,left:5,top:15},400);
     }
   })
   $(".passwordBox").click(function(){
-    $(":password").animate({height:26},400);
+    $(".passwordBox").children("input").animate({height:30},400);
     $("label.passWord").animate({fontSize:12,left:0,top:0},200);
-    $("label.passWord").stop().animate({fontSize:12,left:0,top:6},300);
+    $("label.passWord").stop().animate({fontSize:12,left:0,top:5},300);
   })
-  $(":password").blur(function(){
-    var passwordArr=$(":text").val();
+  $(".passwordBox").children("input").blur(function(){
+    var passwordArr=$(this).val();
     if (passwordArr.length == 0) {
-      $(":password").animate({height:2},400);
+      $(this).animate({height:2},400);
       $("label.passWord").stop().animate({fontSize:18,left:5,top:15},400);
     }
   })
   
   $("form").submit(function(){
-    var passwordArr=$(":password").val();
-    var usernameArr=$(":text").val();
+    var passwordArr=$(".passwordBox").children("input").val();
+    var usernameArr=$(".usernameBox").children("input").val();
     if (usernameArr.length == 0) {
       alert("用户名不能为空");
       return false;
