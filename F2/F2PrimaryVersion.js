b7876a8bed20c88fade455c35c283277b59b8bb2
barrybang <2668212523@qq.com>
2016-08-14 00:56:30 +0800

    改用css动画 对input监听 对label添加class 数据验证和css样式有了小修改

--- F2/F2PrimaryVersion.js
+++ F2/F2PrimaryVersion.js
@@ -2,53 +2,47 @@ $(document).ready(function(){
   $(".usernameBox").children("input")
   /*点击username 输入框弹开*/
     .focus(function(){
-      $(this).animate({height:35},400);
-      $("label.userName").animate({fontSize:10,left:0,top:0},200);
-      $("label.userName").stop().animate({fontSize:10,left:0,top:6},300);
+      $(this).siblings("label").stop().addClass("labelStart");
     })
   /*当点击外部区域 且输入框内没有内容时 输入框缩小*/
     .blur(function(){
       var usernameArr=$(this).val();
       if (usernameArr.length == 0) {
-        $(this).animate({height:3},400);
-        $("label.userName").stop().animate({fontSize:15,left:10,top:20},400);
+        $(this).siblings("label").css({fontSize:10,left:0,top:6});
+        $(this).siblings("label").removeClass("labelStart");
+        $(this).siblings("label").animate({fontSize:15,left:10,top:20},400);
+        $(this).css({height:35});
+        $(this).stop().animate({height:3},400);
       }
     })
-  $(".passwordBox").children("input").
-  /*点击password 输入框弹开*/
-    focus(function(){
-      $(this).animate({height:35},400);
-      $("label.passWord").animate({fontSize:10,left:0,top:0},200);
-      $("label.passWord").stop().animate({fontSize:10,left:0,top:6},300);
-    })
+  $(".passwordBox").children("input")
+    .focus(function(){
+      $(this).siblings("label").stop().addClass("labelStart");
+      })
   /*当点击外部区域 且输入框内没有内容时 输入框缩小*/
     .blur(function(){
       var passwordArr=$(this).val();
       if (passwordArr.length == 0) {
-        $("label.passWord").animate({fontSize:15,left:10,top:20},400);
-        $(this).animate({height:3},200);
+        $(this).siblings("label").css({fontSize:10,left:0,top:6});
+        $(this).siblings("label").removeClass("labelStart");
+        $(this).css({height:35});
+        $(this).siblings("label").animate({fontSize:15,left:10,top:20},400);
+        $(this).animate({height:3},400);
       }
     })
   /*submit的数据验证*/
   $("form").submit(function(){
     var passwordArr=$(".passwordBox").children("input").val();
     var usernameArr=$(".usernameBox").children("input").val();
-    var pattern1=/^[A-Za-z0-9]+$/; /*由数字和字母构成的正则表达式*/
-    var pattern2=/^[a-zA-Z]/;  /*首字符为字母的正则表达式*/
+    var lengthtest=/^[A-Za-z0-9]+$/; /*由数字和字母构成的正则表达式*/
+    var firsttest=/^[a-z]+$/;  /*首字符为小写字母的正则表达式*/
     if (usernameArr.length == 0) {
       alert("用户名不能为空");
       return false;
     }
-    else if (pattern2.test(passwordArr)) {
-      alert("用户名应由字母开头");
+    else if (firsttest.test(passwordArr) && lengthtest.test(passwordArr && usernameArr.length <6 || usernameArr.length >15)) {
+      alert("用户名应由小写字母开头且由6~15位字母及数字组成");
       return false;
-    }
-    else if (pattern1.test(passwordArr)) {
-      alert("用户名应由数字和字母组成");
-      return false;
-    }
-    else if (usernameArr.length <6 || usernameArr.length >15) {
-      alert("用户名长度应为6~15个字符")
     }
     else if (passwordArr.length == 0) {
       alert("密码不能为空");
